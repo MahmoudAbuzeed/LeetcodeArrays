e1a37467f227acd3917f81e3d08a61f4a95bcd68
Mahmoud Abo-Zeed <mahmoud@Mahmoud.Abozeed>
2021-05-08 20:25:47 +0200

    adding remove duplicates solution

---
+++ removeDuplicates.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length == 0) return 0;
+
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] != nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+
+  return i + 1;
+
+  // another answer
+  //   return [...new Set(nums)].length;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
